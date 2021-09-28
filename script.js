043539ec53dddb5d7ad6bc9278b235f6e99a344b
deambulatory <63451208+deambulatory@users.noreply.github.com>
2021-09-28 09:28:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,431 @@
+
+let rules = [ 
+
+"Drink and Nominate / Drink one, then nominate two",
+"Drink and Nominate / Drink two, then nominate one",
+"Drink Two, Redo  / Drink two, then pick another piece",
+"Why Does Drenga Hate Me?  / Drink four",
+"Four and Out  / Drink four. You're now immune from any drink nominations until it's your turn again",
+"Popular  / Nominate one person to drink four",
+"Peer Pressure  / Drink one for every person playing",
+"Cheers Lads  / Guys drink",
+"Girls' Night Out  / Girls drink",
+"Challenge 25 / Youngest person drinks",
+"Hero Time  / Take a shot of the strongest drink you can find",
+"One Night Stand  / Pick someone of the opposite sex and do a shot with them. If there aren't any members of the opposite sex, then everyone does a shot",
+"Rock, Paper, Scissors  / Group game! Everyone the piece taker beats drinks one; the piece taker must drink for the amount of people that beat them",
+"Play Your Cards Right  / Highest card nominates and the lowest drinks. You decide the amount before the cards are dealt. If you don't have a pack of cards, select a random rule ",
+"Nose Game  / When the piece taker holds their finger on their nose, everyone else must. The last person to do so drinks three. Rule expires when the game ends or there's a new nose master",
+"Cold War / Pick someone to take the first drink from whatever drinks you get nominated for the next round; if there's only one drink you both do it",
+"Wingman  / Pick someone to drink with you for the next round. They have to drink whatever you get nominated to drink",
+"The Ol' Switcheroo  / Reverse the direction of play",
+"Chug: Casual Edition / Finish half your drink before your next turn",
+"Southpaw  / On your next turn you have to use your non-dominant hand to take a piece",
+"Piggy in the Middle  / You can't pick a middle piece on your next turn",
+"Pickpocket  / Hold onto this piece. When someone picks a piece that you would like you can swap this piece with them and take theirs",
+"Technical Party  / If there are more than six people in the room, then everyone drinks. If there are fewer than six people, only you drink",
+"Seven  / Heaven. Hands in the air people! Last person to raise both hands drinks two",
+"Neanderthal  / Everyone can only speak using one-syllable words. Pointing and grunting is allowed. Rule lasts for the next three turns",
+"Quick! / The last person to put their finger on the top of their drink downs it. If the group can't decide who this was then everyone drinks two",
+"Weights and Measures / You are responsible for enforcing forfeits and ensuring that all players drink what they should until the end of the game",
+"Question Master  / No one can reply to any questions asked by this player. If anyone responds they must drink one. Lasts for the next three turns",
+"Clairvoyant  / Guess everyone's piece number for the next round. If you are within five (plus or minus) of the correct number they must finish their drink",
+"Grandfather Paradox  / Youngest and oldest people drink",
+"Birthday Paradox  / If two people have the same birthday they must both drink four, otherwise you must drink two",
+"In My Pants  / You need to end every sentence with \"in my pants\" until the next three turns have finished.",
+"Bears / Going clockwise, start counting up from one; any number that has seven in or is a multiple of seven is replaced with \"bears\". Drink two if you get it wrong",
+"Snake Eyes  / If anyone makes eye contact with the piece taker they must drink two. Rule lasts for the next three turns",
+"Hydration  / Get yourself a glass of water to have alongside your drink. You wouldn't want a hangover, right?",
+"Eye for an Eye  / The next time you nominate drinks you must drink an equal amount",
+"Give Two, Redo  / Nominate two drinks, then take another piece",
+"Depersonalisation  / You must refer to yourself in the third person for the next round",
+"Rainbow Warrior  / Pick a colour. Everyone must drink one for every article of clothing they have on that contains that colour",
+"Speed Game  / After this block is placed the next player has 15 seconds to complete their turn. If they fail to do so they must drink three",
+"Ginlleps  / Spell the first name of the person to your left backwards. Make a mistake and drink three",
+"Repellent  / No matter what the person to your left says or does you must actively disagree. Lasts for the next three turns",
+"Healthy Living  / Quickly name any fruit! Now drink for however many letters it contains; if you read all this before naming one drink four",
+"Doomsayer  / Predict who will knock the tower down. If you end up being wrong you must finish half your drink",
+"Lightspeed  / The next person skips their go but drinks three",
+"3rd Graders' Revenge  / If you're not nine years old, drink two",
+"Do Over  / Put this piece back where it came from and get a different one ",
+"You'll Regret That! / The next time someone nominates you two or more drinks you may nominate them three drinks in return",
+"Ring of Recoil / For the next round whenever someone nominates you to drink they must also drink one",
+"Free Pass  / Hold onto this piece. On any future turn you may place this instead of pulling a new piece",
+"Stuck on You  / Pick a \"conjoined twin\" - a player to your right or left. You share a liver: any drinks given to either of you must be split equally for the next round",
+"Splash Damage / People sat next to you must drink two each",
+"Phrase like a Question / Everything all players say needs to sound like it ends in a question mark? Rule lasts for the next three turns?",
+"Social / Everyone playing drinks",
+"The Photographer / The \"photographer\" has a camera (real or pretend). You must count down from three loudly whilst pointing the camera in one place. Anybody not in the photograph drinks",
+"Ready for Battle / When drinking you must hold your drink with two hands. Rule lasts for the next three turns.",
+"Mr. President / When you speak you have to put two fingers to your ear as if you are part of the President's security detail communicating with other personnel. Lasts for three turns",
+"Safety First / Undo your invisible seat belt whenever you get up and put it back on when you sit down. Lasts for the next three turns",
+"Choo Choo! / Whenever you laugh you have to pump your arms back and forth like the wheel rods on a steam engine. Lasts for the next three turns",
+"Lurch / The tallest person must drink two, then nominate two drinks",
+"Risky Business / If the next person is able to remove the first piece they touch, they can nominate four drinks. If they have to move on to another brick, they must drink two",
+"Battle of the Sexes / Everyone whose gender is opposite to yours must drink one",
+"Categories /  ",
+"Categories /  ",
+"Categories /  ",
+"Scripture / For the next three turns, you must end each sentence with a fake chapter and verse as if it were a bible quote e.g. \"does anyone want a shot of vodka? Proverbs, chapter 20, verse 1\"",
+"Party Council  / Everyone collectively decides who should drink three",
+"Beer Police / The person that is least drunk, as voted by the group, drinks two",
+"Out of Juice / The player with the least amount in their drink must finish it",
+"Denunciation / On the count of three all players point at another player. Whoever has the most people pointing at them drinks two. If it is a draw drink one each",
+"Thousand-yard Stare / Have a staring contest with the player on the left. The loser drinks two",
+"1st Day of School / For the next three turns you must raise your hand before speaking. Drink one whenever you forget to do so",
+"Kangled / Before your next turn you need to make up a new word and say it in a sentence. Drink one for each person that notices",
+"Roisin / The shortest person must drink two ",
+"Oktoberfest  / It is now your job to bring new drinks for people when they run out. Do this until it is your turn again",
+"Mutant / All those with blue eyes must drink two; all those with brown eyes can nominate two",
+"Present / Anyone with their phone in their hand right now must drink four",
+"Multiplier / The next drink nomination you receive is doubled",
+"1984 / You can pick a word that is banned for the next round. If anyone says this word they must drink two",
+"You! / Everyone look at the floor. On the count of three look up at another player's eyes. Any players making eye contact with each other must drink two",
+"Welcome! / Anyone that is playing Casual Drenga for the first time must drink two. If you've all played before then everyone drinks",
+"Lydia / Anyone that has spilled a drink this game must drink two. The next person to spill a drink must drink four",
+"Bootcamp / Do twenty reverse lunges or drink three",
+"Bros / High five another player. Everyone else in the game other than you two must drink three",
+"Story Time / Make a story one word at a time. Each player must recite the story from the beginning before adding their word at the end. Drink three if you repeat a word or mess up",
+"Shot Roulette / Fill four shot glasses with water, two with vodka, then mix them up. Going clockwise, each person picks one shot. If you have no shot glasses pick a random rule",
+"Dealer / Deal cards to the rest of the players. Players that receive a red suit must drink two. Players that receive a black suit must nominate two drinks (dealer is immune).",
+"Rapunzel / The player with the longest hair drinks two"
+
+];
+
+let categories = [ 
+
+	"music genres",
+   	"dog breeds",
+	"US states",
+	"car manufacturers",
+	"capital cities",
+];
+
+let title = document.getElementById('title');
+let description = document.getElementById('description');
+let descriptionContainer = document.getElementById('descriptionContainer');
+let counter = document.getElementById("counter");
+
+let instructions = document.getElementById("instructions");
+
+let d_numinput = document.getElementById("d_numinput");
+let d_reset = document.getElementById("refresh");
+let bricks = document.getElementById("bricks");
+
+let ackJS = document.getElementById("ack");
+
+let m_newRule = document.getElementById("newRule");
+let m_random = document.getElementById("random");
+let m_reset = document.getElementById("reset");
+
+let modal = document.querySelector(".modal");
+let yes = document.getElementById("yes");
+let no = document.getElementById("no");
+
+let bricksMoved = 0;
+let gameset = rules;
+let catset = categories;
+
+function shuffle(array) {
+	
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    
+    
+
+    return array;
+
+}
+
+function ack(){
+
+instructions.style.display = "none";
+d_numinput.style.visibility = "visible";
+d_reset.style.visibility = "visible";
+bricks.style.visibility = "visible";
+m_newRule.style.visibility = "visible";
+m_random.style.visibility = "visible";
+m_reset.style.visibility = "visible";
+
+}
+
+function randomRule(){
+	
+	if (gameset.length > 0) 
+	{
+	
+		counter.style.display = "none";
+		//var str = rules[Math.floor((Math.random() * rules.length) + 1)]; stop duplicate rules as confusing...
+		var str = gameset.pop();
+		var res = str.split(" / ");
+		title.innerText = res[0];
+		description.innerText = res[1];
+		m_newRule.innerText = "Bricks Moved: " + bricksMoved;
+		bricks.innerText = "Bricks Moved: " + bricksMoved;
+		d_numinput.value = "";
+		if (res[0].includes("Speed")) countdown();
+		if (res[0].includes("Categories")) categoryRule();	
+	
+	}
+	
+		else 
+				
+		{
+						
+			counter.style.display = "none";
+			description.innerText = "You should realistically never find this message unless you're on a big sesh. Everyone drink five to celebrate. Automatically shuffling rules in 10 seconds..." 
+			title.innerText = "Out of Unique Rules"
+			bricksMoved -= 1;
+			tenSecCountdown();
+				
+		}
+	
+};
+
+function pickRule(){
+	
+	       if (gameset.length == 0)
+        {
+            	
+			if (bricksMoved > 60) 
+				
+				{
+					
+					description.innerText = "You really shouldn't find this. Finish your drink" 
+					title.innerText = "Easter Egg"
+					
+				}
+				
+			
+				else 
+				
+				{						
+					counter.style.display = "none";
+					description.innerText = "You should realistically never find this message unless you're on a big sesh. Everyone drink five to celebrate. Automatically shuffling rules in 10 seconds..." 
+					title.innerText = "Out of Unique Rules"
+					tenSecCountdown();
+				}			
+		
+		            
+        }
+        
+        else
+        
+        {
+		counter.style.display = "none";
+		bricksMoved += 1;
+            	var str = gameset.pop();
+            	var res = str.split(" / ");
+	        title.innerText = res[0];
+            	description.innerText = res[1];
+           	m_newRule.innerText = "Bricks Moved: " + bricksMoved;
+		bricks.innerText = "Bricks Moved: " + bricksMoved;
+           	d_numinput.value = "";
+		if (res[0].includes("Speed")) countdown();
+		if (res[0].includes("Categories")) categoryRule();		
+            
+        }
+	
+};
+
+function categoryRule(){
+	
+	
+	if (catset.length == 0)  
+	
+	{
+		
+	   shuffle(categories);
+	   catset = categories.slice(0);
+		
+	}
+		
+	var str = catset.pop();
+	description.innerText = "Going clockwise quickly name " + str + ". Stop when someone can't name something. They drink three";
+	
+};
+
+function resetGame() {
+	
+	
+	
+	counter.style.display = "none";
+	$('.button').prop('disabled', true);
+	toggleModal();
+};
+
+ackJS.addEventListener('click', ack);
+m_random.addEventListener('click', randomRule);
+m_newRule.addEventListener('click', pickRule);
+d_reset.addEventListener('click', resetGame);
+m_reset.addEventListener('click', resetGame);
+bricks.addEventListener('click', d_numinput.focus);
+$("#title").click(focusInput);
+$("#descriptionContainer").click(focusInput);
+$("#description").click(focusInput);
+yes.addEventListener("click", yesClick);
+no.addEventListener("click", noClick);
+
+function focusInput (){
+	
+	d_numinput.focus();
+};
+
+$("#d_numinput").keydown(function(e) {
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    
+    
+    if (keycode == 48 && d_numinput.value.length == 0 ) event.preventDefault();
+    if (keycode == 48 && d_numinput.value == "Start typing..." ) event.preventDefault();
+    
+   if (keycode == 48 || keycode == 49 || keycode == 50 || keycode == 51 || keycode == 52 || keycode == 53 || keycode == 54 || keycode == 55 || keycode == 56 || keycode == 57 || keycode == 13 || keycode == 8) 
+       
+       {
+             
+     if (keycode == 48 && d_numinput.value == "Start typing..." ) event.preventDefault();     
+     else if (d_numinput.value.length > 2 && d_numinput.value != "Random" ) d_numinput.value = "";
+    
+    
+	if (keycode == '8' && d_numinput.value == "Random"){
+		
+		d_numinput.value = "";
+		
+	}
+	
+    if (keycode == '13') {
+               
+       var rulenumber = parseInt(d_numinput.value);
+                
+        if (rulenumber > 0 && rulenumber < 64 ) pickRule();
+        else if (d_numinput.value == "Random")	randomRule();
+        
+        else if (d_numinput.value.length > 0)
+        {
+			counter.style.display = "none";
+            description.innerText = "Enter a valid rule number (1 - 63)" 
+            title.innerText = "Incorrect rule - drink two"
+            d_numinput.value = ""
+            
+        }    
+     
+    }
+                        
+    }
+    
+    else event.preventDefault();
+    
+    if (keycode == '82' || keycode == '114') {
+        
+        d_numinput.value = "Random"
+        
+    }
+    
+
+ });
+
+var windowHeight = window.innerHeight;
+document.documentElement.style.setProperty('--windowHeight', `${windowHeight}px`);
+var windowWidth = window.innerWidth;
+document.documentElement.style.setProperty('--windowWidth', `${windowWidth}px`);
+
+$( document ).ready(function() {
+	document.getElementsByTagName("html")[0].style.visibility = "visible";
+	document.getElementsByTagName("body")[0].style.visibility = "visible";
+	shuffle(rules);
+	gameset = rules.slice(0);
+	shuffle(categories);
+	catset = categories.slice(0);
+	d_numinput.focus();
+});
+
+window.addEventListener('resize', () => {
+    windowHeight = window.innerHeight;
+    document.documentElement.style.setProperty('--windowHeight', `${windowHeight}px`);
+    
+    windowWidth = window.innerWidth;
+    document.documentElement.style.setProperty('--windowWidth', `${windowWidth}px`);
+    
+    if (windowWidth > 960) d_numinput.focus();
+    
+});
+
+ function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function tenSecCountdown() {
+
+    $('.button').prop('disabled', true);
+	await sleep(10000);
+	shuffle(rules);
+	gameset = rules.slice(0);
+	$('.button').prop('disabled', false);
+	pickRule();
+  
+}
+
+async function countdown() {
+
+    counter.style.display = "flex";
+	counter.style.zIndex = 5;
+
+  // Sleep in loop
+  for (let i = 15; i >0; i--) {
+	  
+	  counter.innerHTML = ""+i; 
+      await sleep(1000);
+    ;
+  }
+  
+  counter.style.display = "none"
+  
+}
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function yesClick(){
+	
+	   $('.button').prop('disabled', false);
+	   shuffle(rules);
+	   gameset = rules.slice(0);
+	   shuffle(categories);
+	   catset = categories.slice(0);
+	   title.innerText = "Casual Drenga!";
+	   description.innerText = "";
+	   bricksMoved = 0;
+	   bricks.innerText = "Bricks Moved: " + bricksMoved;
+	   m_newRule.innerText = "Bricks Moved: " + bricksMoved;
+	   d_numinput.value = "Start typing...";
+	   modal.classList.toggle("show-modal");
+	   d_numinput.focus();
+};
+
+function noClick(){
+	
+	   $('.button').prop('disabled', false);
+	   title.innerText = "Casual Drenga!";
+	   description.innerText = "";
+	   bricksMoved = 0;
+	   bricks.innerText = "Bricks Moved: " + bricksMoved;
+	   m_newRule.innerText = "Bricks Moved: " + bricksMoved;
+	   d_numinput.value = "Start typing...";
+	   modal.classList.toggle("show-modal");
+	   d_numinput.focus();
+};
+
+
